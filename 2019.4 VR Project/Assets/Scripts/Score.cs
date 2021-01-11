f3ef8bbde79d61a0918f8338ee7d093e539b9792
TH home <thiemo.haller@student.uibk.ac.at>
2021-01-11 14:58:41 +0100

    Add XR project
    For now only VR

---
+++ 2019.4 VR Project/Assets/Scripts/Score.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    private TextMesh textMesh;
+
+    private void Awake() {
+        textMesh = GetComponent<TextMesh>();
+    }
+
+    public void ShowScore(int score) {
+        textMesh.text = score.ToString();
+    }
+}
